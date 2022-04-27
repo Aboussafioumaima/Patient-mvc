26298617edae1c09281644875be769d584d1788e
lenovo <oaboussafi@gmail.com>
2022-04-27 05:27:12 +0200

    pagination avec recherche des patients avec nom

--- src/main/java/com/oum/patientmvc/repositories/PatientRepository.java
+++ src/main/java/com/oum/patientmvc/repositories/PatientRepository.java
@@ -1,8 +1,11 @@
 package com.oum.patientmvc.repositories;
 
 import com.oum.patientmvc.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Page<Patient> findByNomContains(String kw , Pageable pageable);
 
 }
--- src/main/java/com/oum/patientmvc/web/PatientController.java
+++ src/main/java/com/oum/patientmvc/web/PatientController.java
@@ -4,9 +4,12 @@ import com.oum.patientmvc.entities.Patient;
 import com.oum.patientmvc.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,11 +18,15 @@ import java.util.List;
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping(path = "/index")
-    public String patients(Model model){
-        List<Patient> patients=patientRepository.findAll();
-        model.addAttribute("listPatients",patients);
-
-
+    public String patients(Model model,
+                           @RequestParam(name = "page",defaultValue = "0") int page,
+                           @RequestParam(name = "size",defaultValue = "5") int size,
+                           @RequestParam(name = "keyword",defaultValue = "") String keyword)
+    {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(keyword,PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("keyword",keyword);
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         return "patient";
     }
 
